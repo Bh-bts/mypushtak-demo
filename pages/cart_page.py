c400782ea20be9f1472b4cf58d3ee99fc232bc33
bh-bts <thumarbhavin.786@gmail.com>
2024-06-18 14:09:16 +0530

    Fix wishlist test

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -7,7 +7,7 @@ class CartPage(BasePage):
     # Locators
     total_price_text = (By.CSS_SELECTOR, "span[id='BookPricediv']")
 
-    # Get the total price of items in the cart
+    # Get the total price of items in the cart.
     def get_total_price(self):
         price_text = self.get_element_text(self.total_price_text)
         price_number = price_text.replace('₹', '').replace(',', '').strip()
